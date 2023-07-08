c93a330ad303575ea50a1c932a638dae7e2d49b0
Konrad <komocarski@gmail.com>
2023-07-08 20:08:44 +0200

    initial front commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MicroWarehouse
 Project made as a part of Languages and Programming Tools II course at the University of Warsaw (microservises group).
+
+Before You start the front app do the following:
+1. npm install -g create-react-app
+2. npm install react-router-dom @types/react-router-dom
+3. cd from 
+4. npm start
---
+++ front/src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import Nav from "./components/Nav";
+import Menu from "./components/Menu";
+import Products from "./products/Products";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+function App() {
+    return (
+        <div className="App">
+            <Nav/>
+
+            <div className="container-fluid">
+                <div className="row">
+                    <Menu/>
+
+                    <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+
+                        <BrowserRouter>
+                            <Routes>
+                                <Route path='/products' Component={Products}/>
+                            </Routes>
+                        </BrowserRouter>
+
+                    </main>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
